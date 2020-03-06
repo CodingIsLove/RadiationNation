c3500b3c8165a5bd36f41b0aa3503fa4c8fccf80
Chris Germann <christopher.germann@swisscom.com>
2020-03-06 14:00:56 +0100

    Removed the fucking typescript from this project

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,9 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -20,8 +18,8 @@ module.exports = {
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        "**/__tests__/*.js?(x)",
+        "**/tests/unit/**/*.spec.js?(x)"
       ],
       env: {
         mocha: true
--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -1,4 +1,7 @@
 <template>
+    <div class="Game">
+        <h1>This here is a game, how sweet is this</h1>
+    </div>
     
 </template>
 
