35848ad1a5471ef3fb8bd65927891d53a5fd23f0
Chris Germann <christopher.germann@swisscom.com>
2020-03-21 19:23:15 +0100

    Got some first results from working with canvas, but there is still a lot to do

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -1,13 +1,52 @@
 <template>
     <v-app>
         <h1>Here will be the Game view</h1>
+        <div class="ratioContainer">
+            <canvas id="radiationNation" height="600px" width="900px" />
+        </div>
+        <v-btn @click="hello">Random Background Color</v-btn>
+        <v-btn>draw random circle</v-btn>
+        <v-btn @click="square">draw random square</v-btn>
     </v-app>
 </template>
 
 <script>
+    const Board = require('./canvas/Board')
+   //const GameInstance = require('./canvas/GameInstance')
 export default {
-  name: "Game"
+  name: "Game",
+    data: function(){
+     return {
+         gameField: Object,
+         gameInstance: Object,
+         canvas: Object
+     }
+    },
+    methods:{
+     hello: function () {
+         console.log('Hello world fuckers')
+     },
+        square: function () {
+         this.gameField.drawGrid()
+        }
+    },
+    mounted() {
+      console.log("Instance was mounted")
+        this.canvas = document.getElementById('radiationNation')
+        this.gameField= new Board(this.canvas,15);
+        //this.gameInstance = new GameInstance('radiationNation');
+        //this.gameInstance.checkCompatibility()
+    }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+    .ratioContainer{
+        display:flex;
+        justify-content: center;
+    }
+
+    #radiationNation{
+        width: 100%;
+    }
+</style>
---
+++ frontend/src/components/canvas/Tile.js
@@ -0,0 +1,65 @@
+
+module.exports = class Tile{
+    /**
+     * @param tileAmount defines the amount of tiles in x and y direction
+     * @param canvas the canvas on which to draw
+     * @param perspective you can choose between "PLANAR" or "ISOMETRIC"
+     */
+    constructor(x,y,tileAmount,canvas,margin,perspective) {
+        this.x = x //Later apply transformation to this tile
+        this.y = y
+        this.margin = margin
+        this.tileAmount = tileAmount
+        this.canvas = canvas
+        this.stdWidth = this.canvas.width// This can be any random value
+        this.x_trafo =x
+        this.y_trafo = y
+        this.perspective = perspective
+        this.ctx = this.canvas.getContext('2d')
+
+        switch (perspective) {
+            case "PLANAR":
+                this.planarTransformation()
+                break;
+            case "ISOMETRIC":
+                this.isometricTransformation()
+                break
+            default:
+                throw "Your perspective is not defined!"
+        }
+    }
+
+    drawTile(){
+        console.log(`Drawing tile at position: (${this.x},${this.y})`)
+        console.log(`Current perspective is: ${this.perspective}`)
+
+        // Convert the base coordinates into coordinates of the choosen coordinate system
+        if(this.perspective === "PLANAR"){
+            this.ctx.fillRect(this.x_trafo,this.y_trafo,this.stdWidth-3,this.stdWidth-3)
+            this.ctx.strokeRect(this.x_trafo,this.y_trafo,this.stdWidth-3,this.stdWidth-3)
+        }else{
+            console.log("Now it should make the isometric transformation")
+        }
+    }
+
+    //----------------------------------- Transformations ----------------------------------
+    planarTransformation(){
+        this.stdWidth = (this.canvas.height - this.margin) / this.tileAmount
+        this.x_trafo = this.x*this.stdWidth + this.margin/2
+        this.y_trafo = this.y*this.stdWidth + this.margin/2
+        console.log(this.stdWidth)
+    }
+
+    isometricTransformation(){
+        this.stdWidth =(this.canvas . this.margin)/(2*Math.sqrt(2))
+    }
+
+    //----------------------------------- TileStates ----------------------------------
+    applyStandard(){
+        this.ctx.beginPath()
+        this.ctx.fillStyle = 'black'
+        this.ctx.strokeStyle='white'
+    }
+
+
+}
