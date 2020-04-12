f7f3c662b38c6d537ea239f348a75c43fa44a841
Chris Germann <christopher.germann@swisscom.com>
2020-04-12 14:06:22 +0200

    Removed unnecessary file and added connection options for mongo to .env

--- backend/src/routes/errorRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const errorRouter = express.Router();
-
-errorRouter.get("/*",(req,res)=>{
-    res.send(`This route does not exist. The used ip was ${req.ip}`)
-});
-
-export {errorRouter};
