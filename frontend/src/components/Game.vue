4eff508e97dfc5233823258a551daffe649ab6af
Chris Germann <christopher.germann@swisscom.com>
2020-05-16 11:35:53 +0200

    resolved shitty conflicts

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -1,52 +1,27 @@
 <template>
     <v-app>
-        <h1>Here will be the Game view</h1>
-        <div class="ratioContainer">
-            <canvas id="radiationNation" height="600px" width="900px" />
-        </div>
-        <v-btn @click="hello">Random Background Color</v-btn>
-        <v-btn>draw random circle</v-btn>
-        <v-btn @click="square">draw random square</v-btn>
+        <div>Welcome {{}}</div>
+        <canvas>
+
+        </canvas>
     </v-app>
 </template>
 
 <script>
-    const Board = require('./canvas/Board')
-   //const GameInstance = require('./canvas/GameInstance')
 export default {
   name: "Game",
-    data: function(){
-     return {
-         gameField: Object,
-         gameInstance: Object,
-         canvas: Object
-     }
+    data:function () {
+      return{
+
+      }
     },
-    methods:{
-     hello: function () {
-         console.log('Hello world fuckers')
-     },
-        square: function () {
-         this.gameField.drawGrid()
-        }
+    computed:{
+
     },
-    mounted() {
-      console.log("Instance was mounted")
-        this.canvas = document.getElementById('radiationNation')
-        this.gameField= new Board(this.canvas,15);
-        //this.gameInstance = new GameInstance('radiationNation');
-        //this.gameInstance.checkCompatibility()
+    mounted(){
+      console.log("mounted mounted mounted mounted")
     }
 };
 </script>
 
-<style scoped>
-    .ratioContainer{
-        display:flex;
-        justify-content: center;
-    }
-
-    #radiationNation{
-        width: 100%;
-    }
-</style>
+<style scoped></style>
