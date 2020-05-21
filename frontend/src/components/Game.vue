a4084724787424586e8f624144f81788762e9e76
Ben <31846571+BenWyss@users.noreply.github.com>
2020-05-22 00:23:19 +0200

    Added GS Canvas parameters and grid

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -15,13 +15,22 @@
         data() {
             return {
                 vueCanvasGS: {},
-                vueCanvasCP: {},
                 ctxGS: {},
+                amountOfColumns: 8,
+                amountOfRows: 8,
+                rectGS: undefined,
+                rectGSTileWidth: 0,
+                rectGSTileHeight: 0,
+                gsTileHeight: 0,
+                gsTileWidth: 0,
+
+                vueCanvasCP: {},
+
                 ctxCP: {},
                 cpTileWidth: 0,
                 amountCpTiles: 4,
                 rectCP: undefined,
-                rectTileWidth: 0
+                rectCPTileWidth: 0
             }
         },
         computed: {
@@ -45,13 +54,44 @@
             this.ctxGS = ctxGS;
             this.ctxCP = ctxCP;
             // Canvas / CP is used for drawing only
+            // CP
             this.cpTileWidth = this.vueCanvasCP.width / this.amountCpTiles;
+            // GS
+            this.gsTileWidth = this.vueCanvasGS.width / this.amountOfRows;
+            this.gsTileHeight = this.vueCanvasGS.height / this.amountOfColumns;
             // Rect is the actual size (used for position events, e.g. X and Y)
+            // CP
             this.rectCP = this.vueCanvasCP.getBoundingClientRect();
-            this.rectTileWidth = this.rectCP.width / this.amountCpTiles;
+            this.rectCPTileWidth = this.rectCP.width / this.amountCpTiles;
+            // GS
+            this.rectGS = this.vueCanvasGS.getBoundingClientRect();
+            this.rectGSTileWidth = this.rectGS.width / this.amountOfColumns;
+            this.rectGSTileHeight = this.rectGS.height / this.amountOfRows;
+
+            // Initialize Canvas
             this.initializeCP();
+            this.initializeGS();
         },
         methods: {
+            initializeGS: function() {
+                // 1. Draw Grid
+                // ---- Canvas Setup
+                for (let j = 0; j < this.vueCanvasGS.width; j += this.gsTileWidth) {
+                    for (let k = 0; k < this.vueCanvasGS.height; k += this.gsTileHeight) {
+                        // Draw Tiles
+                        console.log('Drawing GS');
+                        this.ctxGS.rect(j, k, this.gsTileWidth, this.gsTileHeight);
+                        this.ctxGS.fillStyle = 'blue';
+                        this.ctxGS.lineWidth = 1;
+                        this.ctxGS.strokeStyle = 'green';
+                        this.ctxGS.fill();
+                        this.ctxGS.stroke()
+                    }
+                }
+                // 2. Fill the Grid Tiles with the corresponding Tile img/layout/status
+                // 3. Spawn Figures
+                // 4. Add Game functions: Wincondition, Fight, etc.
+            },
             initializeCP: function () {
                 // 1. Draw 4 fields
                 // 2. Color the fields and make them clickable
@@ -61,6 +101,7 @@
                 // ---- Canvas Setup
                 // Setup the panel fields - use Canvas / CP for drawing
                 for (let i = 0; i < this.vueCanvasCP.width; i += this.cpTileWidth) {
+                    console.log('Drawing CS');
                     this.ctxCP.rect(i, 0, this.cpTileWidth, this.vueCanvasCP.height);
                     this.ctxCP.fillStyle = 'orange';
                     this.ctxCP.lineWidth = 1;
@@ -82,8 +123,13 @@
 
                 // Re-Calculate CP Width and Tile Width after Window resizing
                 window.addEventListener('resize',()=>{
+                    // CP
                     this.rectCP = this.vueCanvasCP.getBoundingClientRect();
-                    this.rectTileWidth = this.rectCP.width / this.amountCpTiles;
+                    this.rectCPTileWidth = this.rectCP.width / this.amountCpTiles;
+                    // GS
+                    this.rectGS = this.vueCanvasGS.getBoundingClientRect();
+                    this.gsTileWidth = this.vueCanvasGS.width / this.amountOfRows;
+                    this.gsTileHeight = this.vueCanvasGS.height / this.amountOfColumns;
                 })
 
 
@@ -98,16 +144,16 @@
                 const y = event.clientY - this.rectCP.top;
                 console.log(`You clicked at Position: (${x},${y})`);
                 console.log('The Tile width / Total width is: ' + this.rectCP.width / this.amountCpTiles + ' / ' + this.rectCP.width + '.');
-                if (x < this.rectTileWidth) {
+                if (x < this.rectCPTileWidth) {
                     alert('You pressed on Tile 1  (Left most Tile)');
                 }
-                else if (x < this.rectTileWidth * 2) {
+                else if (x < this.rectCPTileWidth * 2) {
                     alert('You pressed on Tile 2 (Second from Left)');
                 }
-                else if (x < this.rectTileWidth * 3) {
+                else if (x < this.rectCPTileWidth * 3) {
                     alert('You pressed on Tile 3 (Third from Left)');
                 }
-                else if (x < this.rectTileWidth * 4) {
+                else if (x < this.rectCPTileWidth * 4) {
                     alert('You pressed on Tile 4 (Right most Tile)');
                 }
                 else {
