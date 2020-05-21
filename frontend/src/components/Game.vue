62fd5b3506152d2b2520abf82045f2629fc90b4c
Ben <31846571+BenWyss@users.noreply.github.com>
2020-05-21 23:54:30 +0200

    Canvas can now be drawn and mouse position is correctly tracked.

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -20,6 +20,8 @@
                 ctxCP: {},
                 cpTileWidth: 0,
                 amountCpTiles: 4,
+                rectCP: undefined,
+                rectTileWidth: 0
             }
         },
         computed: {
@@ -42,18 +44,22 @@
             this.vueCanvasCP = canvasCP;
             this.ctxGS = ctxGS;
             this.ctxCP = ctxCP;
+            // Canvas / CP is used for drawing only
             this.cpTileWidth = this.vueCanvasCP.width / this.amountCpTiles;
+            // Rect is the actual size (used for position events, e.g. X and Y)
+            this.rectCP = this.vueCanvasCP.getBoundingClientRect();
+            this.rectTileWidth = this.rectCP.width / this.amountCpTiles;
             this.initializeCP();
-            this.method();
         },
         methods: {
             initializeCP: function () {
                 // 1. Draw 4 fields
-                // 2. Color the fields and make them clickabe
+                // 2. Color the fields and make them clickable
                 // 3. Set images into the fields
                 // 4. Add the price tag for each element
 
-                // Setup the panel fields
+                // ---- Canvas Setup
+                // Setup the panel fields - use Canvas / CP for drawing
                 for (let i = 0; i < this.vueCanvasCP.width; i += this.cpTileWidth) {
                     this.ctxCP.rect(i, 0, this.cpTileWidth, this.vueCanvasCP.height);
                     this.ctxCP.fillStyle = 'orange';
@@ -63,31 +69,49 @@
                     this.ctxCP.stroke()
                 }
 
-                // Add Event Listener
+                // ---- Add Event Listener
+                // Onmousemove-Event on the CP
                 this.vueCanvasCP.addEventListener('mousemove', (event) => {
                     this.move(event)
                 });
 
+                // Onclick-Event for clicking on the CP
                 this.vueCanvasCP.addEventListener('click',(event)=>{
                     this.cpClick(event);
+                });
+
+                // Re-Calculate CP Width and Tile Width after Window resizing
+                window.addEventListener('resize',()=>{
+                    this.rectCP = this.vueCanvasCP.getBoundingClientRect();
+                    this.rectTileWidth = this.rectCP.width / this.amountCpTiles;
                 })
+
+
             },
             move:function(event){
-                console.log(`Your current position is: (${event.clientX},${event.clientY})`)
+                const x = event.clientX - this.rectCP.left;
+                const y = event.clientY - this.rectCP.top;
+                console.log('x: ' + x + ' y: ' + y);
             },
-            cpClick:function(e){
-                console.log(`You clicked at position is: (${e.clientX},${e.clientY})`);
-                console.log("the tile width is:" +this.cpTileWidth);
-                if(e.clientX < this.cpTileWidth) {
-                    alert(`You pressed tile 1`)
-                }else if(e.clientX < this.cpTileWidth*2) {
-                    alert(`You pressed tile 2`)
-                }else if(e.clientX < this.cpTileWidth*3) {
-                    alert(`You pressed tile 3`)
-                }else if(e.clientX < this.cpTileWidth*4) {
-                    alert(`You pressed the tile 4`)
-                }else{
-                    alert(`Oh Geez this should not happen`)
+            cpClick:function(event){
+                const x = event.clientX - this.rectCP.left;
+                const y = event.clientY - this.rectCP.top;
+                console.log(`You clicked at Position: (${x},${y})`);
+                console.log('The Tile width / Total width is: ' + this.rectCP.width / this.amountCpTiles + ' / ' + this.rectCP.width + '.');
+                if (x < this.rectTileWidth) {
+                    alert('You pressed on Tile 1  (Left most Tile)');
+                }
+                else if (x < this.rectTileWidth * 2) {
+                    alert('You pressed on Tile 2 (Second from Left)');
+                }
+                else if (x < this.rectTileWidth * 3) {
+                    alert('You pressed on Tile 3 (Third from Left)');
+                }
+                else if (x < this.rectTileWidth * 4) {
+                    alert('You pressed on Tile 4 (Right most Tile)');
+                }
+                else {
+                    alert('Something went wrong! You pressed outside of the Canvas!');
                 }
             }
         }
