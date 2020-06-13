b265cdf0996487e05ace2b37dcbd9f463194cdbf
Chris Germann <christopher.germann@swisscom.com>
2020-06-13 14:09:45 +0200

    Added example tests for the FFHS preasentation

---
+++ backend/src/tests/ffhsPraesiTest.ts
@@ -0,0 +1,21 @@
+import chai from 'chai'
+import {describe, it, before} from "mocha";
+import rest from 'restler'
+import {wipeGameRoom, wipeUser} from '../config/setupDb'
+
+const should = chai.should()
+
+
+describe('Beispiel für FFHS Präsentation', function () {
+    it('This can', (done) => {
+        throw new Error('NOT IMPLEMENTED YET')
+    })
+    it('be used', (done) => {
+        throw new Error('NOT IMPLEMENTED YET')
+    })
+    it('as a ', (done) => {
+        throw new Error('NOT IMPLEMENTED YET')
+    })
+    it('todolist ', (done) => {
+        throw new Error('NOT IMPLEMENTED YET') })
+})
