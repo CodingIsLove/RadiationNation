ce9621c50a186b8bb98701c7469f9a8cc08a24fe
Chris Germann <christopher.germann@swisscom.com>
2020-06-15 02:38:28 +0200

    Chat is working fine

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -9,13 +9,13 @@
             </v-card-title>
 
             <v-list class="list-group list-group-flush text-left" id="chatMessages">
-                <v-list-item-group class="list-group-item" v-for="message in chat.messageTable" :key="message.id">
+                <v-list-item-group class="list-group-item" v-for="message in messageTable" :key="message.id">
                     <span>{{message.username}}: {{message.message}}</span>
                 </v-list-item-group>
             </v-list>
 
             <v-card-actions class="card-actions" id="chatAction">
-                <v-text-field class="float-left" id="textfield" type="text" v-model="chat.message"
+                <v-text-field class="float-left" id="textfield" type="text" v-model="message"
                               v-bind:placeholder="$t('enterMessage')"></v-text-field>
                 <v-btn class="v-btn grey white--text float-right" @click="sendMessage">{{$t("send")}}</v-btn>
             </v-card-actions>
@@ -29,19 +29,20 @@
     export default {
         data() {
             return {
-                chat: {
-                    message: "",
-                    messageTable: [],
-                    username: "",
-                },
                 chatSocket: null,
-                roomId: this.$route.params.gameRoom || 'global'
+                roomId: this.$route.params.gameRoom || 'global',
+                message:"",
+                messageTable:[]
             }
         },
         computed: {
             connections: function () {
                 return this.$store.getters.amountOfClients
+            },
+            username: function () {
+                return this.$store.getters.userName
             }
+
         },
         methods: {
             newSocket() {
@@ -52,7 +53,7 @@
                 });
 
                 this.chatSocket.on('newMessage', (data) => {
-                    this.chat.messageTable.push(data)
+                    this.messageTable.push(data)
                 });
 
                 this.chatSocket.on('userUpdate', (data) => {
@@ -64,21 +65,23 @@
                 })
             },
             sendMessage() {
-                this.chatSocket.emit('sendMessage', this.chat);
-                this.chat.messageTable.push({
-                    message: this.message,
-                    username: this.username,
-                });
-                this.clearTextField();
-                console.log('Sent message to socket')
+                if (this.message !== "") {
+                    this.chatSocket.emit('sendMessage', {
+                        message:this.message,
+                        username:this.username
+                    });
+                    this.clearTextField();
+                    console.log('Sent message to socket')
+                }
             },
             clearTextField() {
                 document.getElementById("textfield").value = "";
-                this.chat.message = "";
+                this.message = "";
             }
         },
         mounted() {
             this.newSocket();
+
             console.log(this.connections)
         }
     };
